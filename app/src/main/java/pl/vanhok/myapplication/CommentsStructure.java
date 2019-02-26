d61d5c79b7247a0f19f08f64c73c7fd0ed5b641b
Vnhk <berluk12@gmail.com>
2019-02-26 21:30:27 +0100

    udpate comments design in news

--- app/src/main/java/pl/vanhok/myapplication/CommentsStructure.java
+++ app/src/main/java/pl/vanhok/myapplication/CommentsStructure.java
@@ -1,6 +1,7 @@
 package pl.vanhok.myapplication;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,15 +23,21 @@ class CommentsStructure{
         tTime.setText(comment.getPostTime());
         tCommentText.setText(comment.getCommentText());
 
+        tAuthor.setBackgroundColor((v.getResources().getColor(R.color.commentContent)));
+        tTime.setTextColor(v.getResources().getColor(R.color.commentContent));
+        tCommentText.setTextColor(Color.WHITE);
+
         tAuthor.setTextColor(comment.getNickColor());
         tVotes.setTextColor(comment.getVotesColor());
 
         linearLayout = new LinearLayout(v);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPaddingRelative(10,10,10,10);
         linearLayout.addView(tAuthor);
         linearLayout.addView(tTime);
         linearLayout.addView(tVotes);
         linearLayout.addView(tCommentText);
+
     }
 
     LinearLayout getLinearLayout() {
